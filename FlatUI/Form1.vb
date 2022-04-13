89692d50108cc56e4c9e3d89bfe42a76b43833ea
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-13 17:48:35 +0800

    Initial commit

---
+++ FlatUI/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.CenterToScreen()
+    End Sub
+
+    Private Sub Panel2_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Panel2.Paint
+
+    End Sub
+
+    Private Sub exitLbl_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles exitLbl.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub MinBtn_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MinBtn.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+End Class
